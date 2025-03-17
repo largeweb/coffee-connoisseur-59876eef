fff8d587b3487c81c3c2bd0bfb411e092d5512a6
largeweb <77933287+largeweb@users.noreply.github.com>
2025-03-17 17:11:35 -0400

    Add data/coffees.ts

---
+++ data/coffees.ts
@@ -0,0 +1,36 @@
+// data/coffees.ts
+
+export const coffees = [
+  {
+    id: 1,
+    origin: "Ethiopia Yirgacheffe",
+    roast: "Light",
+    aroma: "Floral",
+    flavor: "Citrus, Bergamot",
+    body: "Silky",
+  },
+  {
+    id: 2,
+    origin: "Colombia Supremo",
+    roast: "Medium",
+    aroma: "Nutty",
+    flavor: "Chocolate, Caramel",
+    body: "Medium",
+  },
+  {
+    id: 3,
+    origin: "Sumatra Mandheling",
+    roast: "Dark",
+    aroma: "Earthy",
+    flavor: "Dark Chocolate, Spice",
+    body: "Full",
+  },
+  {
+    id: 4,
+    origin: "Kenya AA",
+    roast: "Light",
+    aroma: "Fruity",
+    flavor: "Black Currant, Winey",
+    body: "Bright",
+  },
+];
